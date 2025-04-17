8da4cdc3fd115866072b88fae15743452281af3a
kounou25 <kounougilbert288@gmail.com>
2025-04-17 16:15:45 +0100

    test nouvelle page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import TechnicalDepartmentSection from "./pages/Techniques";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,9 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Index />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="/techniques" element={<TechnicalDepartmentSection />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        {/* Catch-all route for 404 Not Found */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </TooltipProvider>
