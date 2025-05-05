b25967aea895e956c7d5712e927c252480241d4e
kounou25 <kounougilbert288@gmail.com>
2025-05-05 10:51:49 +0100

    suppression de du bouton voir plus sur la section posts

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -185,7 +185,7 @@ const PostSection = () => {
           </div>
         </div>
 
-        <div className="text-center mt-8">
+        {/* <div className="text-center mt-8">
           <Button 
             className="bg-[#2E5A27] hover:bg-[#2E5A27]/80 text-white transition-colors"
             onClick={() => navigate('/blog')}
@@ -193,7 +193,7 @@ const PostSection = () => {
             View All Posts
             <ArrowRight className="ml-2 h-4 w-4" />
           </Button>
-        </div>
+        </div> */}
       </div>
     </section>
   );
